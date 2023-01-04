1ee1adca40dcbc6d0b9953bbd52a6f9255ab0970
Abayomi Aluko <franicons@gmail.com>
2023-01-04 14:23:36 +0100

    feat:development
    * build landing page

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="max-w-5xl space-y-8 mx-auto my-36 px-5 lg:px-10">
+    <!--    Banner ctx-->
     <h3 class="font-bold text-slate-800 font-sans lg:text-6xl text-5xl">
       A smarter and simpler way to map Flowcharts
     </h3>
     <!--    Banner keypoint-->
     <div class="mb-4 flex flex-col text-left space-y-3 font-light">
-      <div class="flex space-x-2">
-        <div class="pt-1"><img src="/images/edit.svg" alt=""></div>
-        <div class="text-slate-600">Easy-to-use editor with automatic flow layout</div>
+      <div class="flex space-x-2" v-for="point in data.keypoint" :key="point.ctx">
+        <div class="pt-1"><img :src="`/images/${point.img}`" alt="" class=""></div>
+        <div class="text-slate-600"> {{ point.ctx }}
+          <template v-if="point.link_1 && point.link_2 && point.link_3">
+            <span class="link">Notion</span>,
+            <span class="link"> Teams </span> and <span class="link">Confluence</span>
+          </template>
+        </div>
       </div>
-      <div class="flex space-x-2">
-        <div class="pt-1"><img src="/images/global.svg" alt="" class=""></div>
-        <div class="text-slate-600">Real-time collaboration and multiple sharing-options</div>
-      </div>
-      <div class="flex space-x-2">
-        <div class="pt-1"><img src="/images/integrations.svg" alt="" class=""></div>
-        <div class="text-slate-600"> Useful integrations with tools like
-          <span class="link">Notion</span>,
-          <span class="link"> Teams </span> and <span class="link">Confluence</span></div>
-      </div>
-
+    </div>
+    <!--    Button -->
+    <div class="space-y-6">
+      <button class="btn-blue">Get started for free</button>
     </div>
   </div>
 </template>
 
 <script>
+import keypoint from "@/mock/data"
+
 export default {
   name: "Banner",
+  data() {
+    return {
+      data: keypoint
+    }
+  },
 };
 </script>
 
