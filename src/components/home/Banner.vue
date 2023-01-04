5803ffb3649ee575983ba46b6907533e3fd4b935
Abayomi Aluko <franicons@gmail.com>
2023-01-04 11:48:52 +0100

    feat:development
    * build landing page

---
+++ src/components/home/Banner.vue
@@ -0,0 +1,13 @@
+<template>
+<div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "Banner"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class=""></div>
+  <section id="Banner-section">
+  </section>
 </template>
 
 <script>
