9473d6394e053ebd9062f096323a44191f1c6199
Abayomi Aluko <franicons@gmail.com>
2023-01-10 17:54:18 +0100

    feat:development
    * build landing page

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -124,10 +124,10 @@
       <button class="btn-blue mt-5">Get started</button>
     </div>
   </section>
-  <footer class="py-28 max-w-6xl mx-auto">
+  <footer class="py-28 p-5 max-w-6xl mx-auto">
     <div class="flex flex-col lg:flex-row  items-start">
-      <h1 class=" font-extrabold text-3xl pr-36">Shifta</h1>
-      <div class="flex space-x-20 flex-col lg:flex-row">
+      <h1 class=" font-extrabold text-3xl">Shifta</h1>
+      <div class="flex md:space-x-20 space-y-12 md:mx-auto md:space-y-0 flex-col md:flex-row">
         <div class="space-y-2">
           <h1 class="font-semibold">Product</h1>
           <div class="text-sm font-semibold group text-gray-800 space-y-3">
@@ -138,12 +138,11 @@
           </div>
         </div>
         <div class="space-y-2">
-          <h1 class="font-semibold">Product</h1>
-          <div class="text-sm font-semibold group text-gray-800 space-y-3">
-            <a href="" class="group-hover:text-blue-700">Features</a>
-            <p>Why ShiftA?</p>
-            <p>Integrations</p>
-            <p>Templates</p>
+          <h1 class="font-semibold">Resources</h1>
+          <div class="text-sm font-semibold text-gray-800 space-y-3">
+            <p><a href="" class="hover:text-blue-700">Customer cases</a></p>
+            <p><a href="" class="hover:text-blue-700">Blog</a></p>
+            <p><a href="" class="hover:text-blue-700">Support</a></p>
           </div>
         </div>
         <div class="space-y-2">
@@ -162,6 +161,9 @@
             <p><a href="" class="hover:text-blue-700">Book a demo</a></p>
           </div>
         </div>
+      </div>
+      <div class="flex flex-col">
+        <p class="font-bold">Newsletter</p>
       </div>
     </div>
   </footer>
